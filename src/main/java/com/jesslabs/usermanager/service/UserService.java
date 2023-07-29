f1ca94a3118b14abd3e0493800deae13830a14e0
JessBarbosa27 <barbosajess27@gmail.com>
2023-07-29 22:10:28 +0530

    Added ApiConstant

--- src/main/java/com/jesslabs/usermanager/service/UserService.java
+++ src/main/java/com/jesslabs/usermanager/service/UserService.java
@@ -6,29 +6,10 @@ import org.springframework.stereotype.Service;
 @Service
 public interface UserService {
 
-    /**
-     * @param userAddRequestDTO
-     * @return
-     * @throws Exception
-     */
     AddUserResponseDTO addUser(AddUserRequestDTO userAddRequestDTO) throws Exception;
 
 
-    /**
-     * @param userUpdateRequestDTO
-     * @return
-     * @throws Exception
-     */
     UpdateUserResponseDTO updateUser(UpdateUserRequestDTO userUpdateRequestDTO) throws Exception;
 
-    /**
-     * @param pageSize
-     * @param pageNo
-     * @param sortBy
-     * @param name
-     * @param username
-     * @param role
-     * @return
-     */
     GetUsersPagedDTO getUsers(Integer pageSize, Integer pageNo, String sortBy, String name, String username, String role);
 }
