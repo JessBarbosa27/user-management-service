d52c2fd0dac95870f8b9d524d8cc62b1c99abb22
JessBarbosa27 <barbosajess27@gmail.com>
2024-12-22 18:18:14 +0000

    feat: Keycloak implementation - added SecurityConfig for keycloak

---
+++ src/main/java/com/jesslabs/user_management/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.jesslabs.user_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Updated API for disabling CSRF
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.decoder(jwtDecoder())
+                        ));
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        // Use Keycloak issuer URL to decode the JWT tokens
+        return JwtDecoders.fromIssuerLocation("http://localhost:8180/realms/user-management-realm");
+    }
+}
