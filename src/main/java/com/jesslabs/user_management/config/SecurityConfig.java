84c5af468205e7100a32751b0d9e4e7c76fdb27a
JessBarbosa27 <barbosajess27@gmail.com>
2024-12-23 22:27:03 +0000

    ref: SecurityConfig anf HealthController

--- src/main/java/com/jesslabs/user_management/config/SecurityConfig.java
+++ src/main/java/com/jesslabs/user_management/config/SecurityConfig.java
@@ -1,17 +1,26 @@
-//package com.jesslabs.user_management.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.oauth2.jwt.JwtDecoder;
-//import org.springframework.security.oauth2.jwt.JwtDecoders;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableMethodSecurity
-//public class SecurityConfig {
-//
+package com.jesslabs.user_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Allow all requests without authentication
+                )
+                .csrf(csrf -> csrf.disable()); // Explicitly disable CSRF
+
+        return http.build();
+    }
+
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //        http
@@ -32,4 +41,4 @@
 //        // Use Keycloak issuer URL to decode the JWT tokens
 //        return JwtDecoders.fromIssuerLocation("http://localhost:8180/realms/user-management-realm");
 //    }
-//}
+}
--- src/main/java/com/jesslabs/user_management/contoller/HealthController.java
+++ src/main/java/com/jesslabs/user_management/contoller/HealthController.java
@@ -1,18 +1,30 @@
 package com.jesslabs.user_management.contoller;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@OpenAPIDefinition(info = @Info(title = "Health controller"), tags = @Tag(name = "Health Controller"))
+@RequestMapping("/api/v1/health")
+@Tag(name = "Health Controller", description = "APIs for health monitoring of the application.")
 public class HealthController {
-    @GetMapping("/health")
+
+    @Operation(
+            summary = "Health Check",
+            description = "Endpoint to verify if the application is running and healthy."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Application is healthy."),
+            @ApiResponse(responseCode = "500", description = "Application is not healthy.")
+    })
+    @GetMapping
     public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.status(HttpStatus.OK).body("Success");
+        return ResponseEntity.status(HttpStatus.OK).body("User Management Service is healthy");
     }
 }
